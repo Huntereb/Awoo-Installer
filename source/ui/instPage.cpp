26b8cc638ca9ab33157ef595a4486dc5f362544c
Huntereb <Huntereb@users.noreply.github.com>
2019-11-13 20:24:30 -0500

    Actually remove awoo on install page if specified

--- source/ui/instPage.cpp
+++ source/ui/instPage.cpp
@@ -31,6 +31,7 @@ namespace inst::ui {
         this->Add(this->installInfoText);
         this->Add(this->installBar);
         this->Add(this->awooImage);
+        if (inst::config::gayMode) this->awooImage->SetVisible(false);
     }
 
     void instPage::onInput(u64 Down, u64 Up, u64 Held, pu::ui::Touch Pos) {
