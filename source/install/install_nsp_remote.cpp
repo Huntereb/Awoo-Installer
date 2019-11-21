da7f15f5ccda25f3ebbf2b96819027447f2397d4
Huntereb <Huntereb@users.noreply.github.com>
2019-11-20 21:42:08 -0500

    "Yes, I want a brick" seems a bit extreme

--- source/install/install_nsp_remote.cpp
+++ source/install/install_nsp_remote.cpp
@@ -105,7 +105,7 @@ namespace tin::install::nsp
 
             if (!Crypto::rsa2048PssVerify(&header.magic, 0x200, header.fixed_key_sig, Crypto::NCAHeaderSignature))
             {
-                int rc = inst::ui::mainApp->CreateShowDialog("Invalid NCA signature detected!", "The software you are trying to install may contain malicious contents!\nOnly install improperly signed software from trustworthy sources!\nThis warning can be disabled in Awoo Installer's settings.\n\nAre you sure you want to continue the installation?", {"Cancel", "Yes, I want a brick"}, false);
+                int rc = inst::ui::mainApp->CreateShowDialog("Invalid NCA signature detected!", "The software you are trying to install may contain malicious contents!\nOnly install improperly signed software from trustworthy sources!\nThis warning can be disabled in Awoo Installer's settings.\n\nAre you sure you want to continue the installation?", {"Cancel", "Yes, I understand the risks"}, false);
                 if (rc != 1)
                     THROW_FORMAT(("The requested NCA (" + tin::util::GetNcaIdString(ncaId) + ") is not properly signed").c_str());
                 declinedValidation = true;
