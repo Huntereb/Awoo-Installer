02cbf03ecdad4f7ecc8a0ea8c70fad2b7e69531a
Huntereb <Huntereb@users.noreply.github.com>
2019-11-22 12:18:26 -0500

    Waiting for a button press isn't really useful during a crash

--- source/main.cpp
+++ source/main.cpp
@@ -13,15 +13,7 @@ int main(int argc, char* argv[])
         main->Prepare();
         main->Show();
     } catch (std::exception& e) {
-        printf("An error occurred:\n%s\n\nPress any button to exit.", e.what());
-
-        u64 kDown = 0;
-
-        while (!kDown)
-        {
-            hidScanInput();
-            kDown = hidKeysDown(CONTROLLER_P1_AUTO);
-        }
+        printf("An error occurred:\n%s", e.what());
     }
     inst::util::deinitApp();
     return 0;
