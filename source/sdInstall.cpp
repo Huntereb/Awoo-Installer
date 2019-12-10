fbbda9651355091f8e427c0d113ed5c73664324a
Huntereb <Huntereb@users.noreply.github.com>
2019-12-09 22:30:40 -0500

    New install finished messages

--- source/sdInstall.cpp
+++ source/sdInstall.cpp
@@ -76,7 +76,7 @@ namespace inst::ui {
 namespace nspInstStuff {
 
     std::string finishedMessage() {
-        std::vector<std::string> finishMessages = {"Enjoy your \"legal backups\"!", "I'm sure after you give the game a try you'll have tons of fun actually buying it!", "You buy gamu right? Nintendo-san thanka-you for your purchase!", "Bypassing DRM is great, isn't it?", "You probably saved like six trees by not buying the game! All that plastic goes somewhere!"};
+        std::vector<std::string> finishMessages = {"Enjoy your \"legal backups\"!", "I'm sure after you give the game a try you'll have tons of fun actually buying it!", "You buy gamu right? Nintendo-san thanka-you for your purchase!", "Bypassing DRM is great, isn't it?", "You probably saved like six trees by not buying the game! All that plastic goes somewhere!", "Nintendo ninjas have been dispatched to your current location.", "And we didn't even have to shove a political ideology down your throat to get here!"};
         srand(time(NULL));
         return(finishMessages[rand() % finishMessages.size()]);
     }
